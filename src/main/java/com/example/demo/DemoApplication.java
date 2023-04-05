93c415538178022b184425fa7d41efebbe8ef548
take-4c <82436962+take-4c@users.noreply.github.com>
2023-04-06 01:44:16 +0900

    logback

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,33 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	private Logger logger = null;
-
 	public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+        ch.qos.logback.classic.Logger log = (ch.qos.logback.classic.Logger)logger;
+        log.setLevel(Level.TRACE); 
+
+        logger.trace("trace message");
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	String sayHello() {
-		
-		this.logger = Logger.getLogger(DemoApplication.class.getName());
-        this.logger.setLevel(Level.SEVERE);
-
-		this.logger.finest("FINESTメッセージ");
-        this.logger.finer("FINERメッセージ");
-        this.logger.fine("FINEメッセージ");
-        this.logger.config("CONFIGメッセージ");
-        this.logger.info("INFOメッセージ");
-        this.logger.warning("WARNINGメッセージ");
-        this.logger.severe("SEVEREメッセージ");
-
 		return "Hello World! This is a sample java program!";
 	}
 }
